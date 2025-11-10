1d93b423222ca031c01436851eeecfd272c60b5b
Taisia <tayavenediktova27@gmail.com>
2025-11-10 23:22:51 +0300

    был создан файл поиска периметра и площади треугольника

---
+++ traingle.py
@@ -0,0 +1,22 @@
+def area(a, b): 
+    """
+    Calculate the area of a triangle.
+    Args:
+        a (float): Base length of the triangle.
+        h (float): Height of the triangle.
+    Returns:
+        float: Area of the triangle.
+    """
+    return a * b / 2 
+
+def perimeter(a, b, c): 
+    """
+    Calculate the perimeter of a triangle.
+    Args:
+        a (float): Length of side a.
+        b (float): Length of side b.
+        c (float): Length of side c.
+    Returns:
+        float: Perimeter of the triangle.
+    """
+    return a + b + c 
