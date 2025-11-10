8104dd4c243d2a27ae0afe5889f0439eb02ef117
Taisia <tayavenediktova27@gmail.com>
2025-11-10 23:27:53 +0300

    был создан файл для поиска периметра и площади

---
+++ square.py
@@ -0,0 +1,20 @@
+def area(a):
+    """
+    Calculate the area of a square.
+    Args:
+        a (float): Side length of the square.
+    Returns:
+        float: Area of the square.
+    """
+    return a * a
+
+
+def perimeter(a):
+    """
+    Calculate the perimeter of a square.
+    Args:
+        a (float): Side length of the square.
+    Returns:
+        float: Perimeter of the square.
+    """
+    return 4 * a
